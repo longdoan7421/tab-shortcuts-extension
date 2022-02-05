04fac5f09811971e758b0be65fcf647f2626ceb2
Long Doan <hoanglong7421@gmail.com>
2022-02-05 15:52:41 +0100

    Refactor old commands and add new commands for toggling sound, duplicating tab and ungrouping tab

--- src/background.js
+++ src/background.js
@@ -1,16 +1,54 @@
-chrome.commands.onCommand.addListener((command) => {
-  let openWithNewGroup = false;
-  if (command === 'open-tab-new-group') {
-    openWithNewGroup = true;
-  }
+chrome.commands.onCommand.addListener((command, tab) => {
+  if (!tab) return;
 
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const selectingTab = tabs[0];
-    chrome.tabs.create({ active: true, index: selectingTab.index + 1 }, (newTab) => {
-      chrome.tabs.group({
-        tabIds: newTab.id,
-        groupId: openWithNewGroup ? undefined : selectingTab.groupId,
-      });
-    });
-  });
+  switch (command) {
+    case 'open-tab-current-group':
+      openTabCurrentGroup(tab);
+      break;
+    case 'open-tab-new-group':
+      openTabNewGroup(tab);
+      break;
+    case 'remove-tab-from-group':
+      removeTabFromGroup(tab);
+      break;
+    case 'toggle-sound':
+      toggleSound(tab);
+      break;
+    case 'duplicate-current-tab':
+      duplicateCurrentTab(tab);
+      break;
+    default:
+      break;
+  }
 });
+
+async function openTabCurrentGroup(currentTab) {
+  const sameGroupTabs = await chrome.tabs.query({ currentWindow: true, groupId: currentTab.groupId });
+
+  const newTab = await chrome.tabs.create({ active: true, index: sameGroupTabs[sameGroupTabs.length - 1].index + 1 });
+  chrome.tabs.group({
+    tabIds: newTab.id,
+    groupId: currentTab.groupId,
+  });
+}
+
+async function openTabNewGroup(currentTab) {
+  const newTab = await chrome.tabs.create({ active: true });
+  chrome.tabs.group({
+    tabIds: newTab.id,
+    groupId: undefined,
+  });
+}
+
+async function removeTabFromGroup(currentTab) {
+  chrome.tabs.ungroup(currentTab.id);
+}
+
+async function toggleSound(currentTab) {
+  let muted = !currentTab.mutedInfo.muted;
+  chrome.tabs.update(currentTab.id, { muted });
+}
+
+async function duplicateCurrentTab(currentTab) {
+  chrome.tabs.duplicate(currentTab);
+}
